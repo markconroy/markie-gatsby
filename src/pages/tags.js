aacc46c49dfda4f6d8534ce38edb165ad1a7685e
Mark Conroy <mark@mark.ie>
2020-11-10 20:58:44 +0000

    theming tags like a tag cloud

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,9 +1,45 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
 
+import styled from 'styled-components'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Container from '../components/container'
+
+const TagsListStyles = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  a {
+    color: var(--color-primary);
+  }
+  a:focus,
+  a:hover {
+    text-decoration: none;
+  }
+  a:focus {
+    outline-width: 2px;
+    outline-offset: 3px;
+  }
+  a:hover {
+    transition: 0.3s;
+    border-bottom: 2px solid;
+    padding-bottom: 0.5rem;
+  }
+  li {
+    margin: 1rem;
+  }
+  li:nth-of-type(2n + 2) a {
+    color: var(--color-secondary);
+  }
+  li:nth-of-type(3n + 3) a {
+    color: var(--color-grey-dark);
+  }
+`
 
 export default function TagsListingPage({ data }) {
   const tags = data.tags.nodes
@@ -14,15 +50,35 @@ export default function TagsListingPage({ data }) {
         <Container>
           <h1>Tags</h1>
           <p>we have {tags.length} tags. They are:</p>
-          <ul>
-            {tags.map(tag => (
-              <li>
-                <Link to={tag.path.alias} key={tag.id}>
-                  {tag.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <TagsListStyles>
+            {tags.map(tag => {
+              const hasArticles =
+                tag.relationships.node__article !== null
+                  ? tag.relationships.node__article.length
+                  : 0
+              const hasSpeaking =
+                tag.relationships.node__speaking !== null
+                  ? tag.relationships.node__speaking.length
+                  : 0
+              const count = hasArticles + hasSpeaking
+              return (
+                <li>
+                  <Link to={tag.path.alias} key={tag.id}>
+                    <span
+                      style={{
+                        display: 'inline-block',
+                        fontSize: `${
+                          count === 1 ? '100' : `${100 + count * 7}`
+                        }%`,
+                      }}
+                    >
+                      {tag.name} <small>({count})</small>
+                    </span>
+                  </Link>
+                </li>
+              )
+            })}
+          </TagsListStyles>
         </Container>
       </>
     </Layout>
@@ -37,6 +93,16 @@ export const TagsListingPageQuery = graphql`
         id
         path {
           alias
+        }
+        relationships {
+          node__article {
+            id
+            title
+          }
+          node__speaking {
+            id
+            title
+          }
         }
       }
     }
