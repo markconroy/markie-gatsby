57caa3f129a2f175cada0370418d101465fe7991
Mark Conroy <mark@mark.ie>
2020-11-10 20:58:57 +0000

    theming menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -4,10 +4,13 @@ import styled from 'styled-components'
 
 const MenuStyles = styled.nav`
   overflow: scroll;
+  margin: 0;
   margin-left: 1rem;
+  padding: 0.5rem;
+  border-radius: var(--border-radius);
+  background-color: var(--color-white);
   -ms-overflow-style: none; /* IE 11 */
   scrollbar-width: none; /* Firefox 64 */
-  background-color: var(--color-white);
   &::-webkit-slider-thumb {
     --webkit-appearance: none;
   }
@@ -20,13 +23,24 @@ const MenuStyles = styled.nav`
   li + li {
     margin-left: 1rem;
   }
-  a {
+  .menu-link {
     color: var(--color-secondary);
+    padding: 0.5rem 1rem;
+    border-radius: var(--border-radius);
   }
-  a:focus,
-  a:hover {
+  .menu-link:focus,
+  .menu-link:hover {
     color: var(--color-primary);
     text-decoration: none;
+  }
+  .menu-link--active {
+    background-color: var(--color-primary);
+    color: var(--color-white);
+  }
+  .menu-link--active:focus,
+  .menu-link--active:hover {
+    background-color: var(--color-secondary);
+    color: var(--color-white);
   }
 `
 
@@ -36,22 +50,58 @@ export default function Menu() {
       <MenuStyles>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              className="menu-link"
+              activeClassName="menu-link--active"
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              className="menu-link"
+              activeClassName="menu-link--active"
+              to="/blog"
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/speaking">Speaking</Link>
+            <Link
+              className="menu-link"
+              activeClassName="menu-link--active"
+              to="/speaking"
+            >
+              Speaking
+            </Link>
           </li>
           <li>
-            <Link to="/tags">Tags</Link>
+            <Link
+              className="menu-link"
+              activeClassName="menu-link--active"
+              to="/tags"
+            >
+              Tags
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              className="menu-link"
+              activeClassName="menu-link--active"
+              to="/about"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              className="menu-link"
+              activeClassName="menu-link--active"
+              to="/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </MenuStyles>
