510e7e85f9fb01f3add29d70d8b00342e65150ff
Mark Conroy <mark@mark.ie>
2020-11-10 20:59:11 +0000

    adding container to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,19 @@ import React from 'react'
 import Layout from '../components/layout'
 import Image from '../components/image'
 import SEO from '../components/seo'
+import Container from '../components/container'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi Marky</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
+    <Container>
+      <h1>Hi Marky</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+        <Image />
+      </div>
+    </Container>
   </Layout>
 )
 
