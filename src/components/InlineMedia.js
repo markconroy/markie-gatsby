144d4d845851763762f9960fdd3efee5ebfcf1ec
Mark Conroy <mark@mark.ie>
2020-12-24 17:40:17 +0000

    hack to override opacity for gatsby-image for inline media, need to figure this out properly

--- src/components/InlineMedia.js
+++ src/components/InlineMedia.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import Img from 'gatsby-image'
+import styled from 'styled-components'
+
+const InlineMediaStyles = styled.div`
+  img[aria-hidden] {
+    opacity: 0 !important;
+    transition: 0 !important;
+  }
+  picture img {
+    opacity: 1 !important;
+    transition: 0 !important;
+  }
+`
 
 export default function InlineMedia({
   mediaType,
@@ -11,7 +23,9 @@ export default function InlineMedia({
   if (mediaType === 'image') {
     return (
       <>
-        <Img fluid={inlineImageSource} alt={inlineImageAlt} key={mediaId} />
+        <InlineMediaStyles className="marky">
+          <Img fluid={inlineImageSource} alt={inlineImageAlt} key={mediaId} />
+        </InlineMediaStyles>
       </>
     )
   }
