e0c48136ae631ffbaae4e6e398b47dd503fae5ff
Mark Conroy <mark@mark.ie>
2021-01-28 21:40:49 +0000

    update PWA items

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,13 +33,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Mark Conroy`,
+        short_name: `markie`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `##006699`,
+        theme_color: `##006699`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/markie-icon.jpg`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-offline`,
