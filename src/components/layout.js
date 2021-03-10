42595e94562ec821f28b8052535ed5f1bf921fd1
Mark Conroy <mark@mark.ie>
2021-03-10 12:09:40 +0100

    stop ie11 quick test

--- src/components/layout.js
+++ src/components/layout.js
@@ -36,16 +36,6 @@ const Layout = ({ children }) => {
   return (
     <>
       <GlobalStyles />
-      <Helmet>
-        <script>
-          {/* {window.MSInputMethodContext &&
-            document.documentMode &&
-            document.write(
-              `<script src="https://cdn.jsdelivr.net/gh/nuxodin/ie11CustomProperties@4.1.0/ie11CustomProperties.min.js"><\x2fscript>`
-            )} */}
-          {console.log('Helmet')}
-        </script>
-      </Helmet>
       <LayoutContainerStyles>
         <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
         <main>{children}</main>
