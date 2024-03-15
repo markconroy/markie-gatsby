697a6b54fa6548671e984830b29a06b8dc1dc89d
Mark Conroy <mark@annertech.com>
2024-03-15 17:27:36 +0100

    background colour for page title

--- src/styles/PageTitleContainer.js
+++ src/styles/PageTitleContainer.js
@@ -5,10 +5,27 @@ const PageTitleContainer = styled.div`
   align-items: center;
   margin-bottom: var(--element-spacing-vertical);
   h1 {
-    color: var(--color-primary--dark);
-    border: 4px solid var(--color-primary--dark);
+    position: relative;
+    display: flex;
+    align-items: center;
     padding: var(--base-spacing);
-    padding: var(--base-spacing);
+    width: calc(100% - 2rem);
+    min-height: 300px;
+    border: 4px solid var(--color-white);
+    color: var(--color-white);
+    background-color: var(--color-primary--dark);
+  }
+  h1::before {
+    position: absolute;
+    z-index: -1;
+    align-self: start;
+    content: '';
+    width: calc(100% - 2rem);
+    height: calc(100% - 2rem);
+    left: 3rem;
+    bottom: -1rem;
+    background-color: var(--color-primary--dark);
+    transform: rotate(-3deg);
   }
   ${props =>
     props.noMarginBottom &&
