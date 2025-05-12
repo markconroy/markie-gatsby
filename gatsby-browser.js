a2c2b3b5b2ab2ba730c9b9c06da82d49021de749
Mark Conroy <mark@mark.ie>
2025-05-12 17:09:13 +0100

    Convert ombed videos from Drupal to youtube embeds

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,16 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
+
+// This runs every time a page is loaded or changed.
+export const onRouteUpdate = () => {
+  // Small delay to ensure DOM is fully available
+  setTimeout(() => {
+    const embeddedVideos = document.querySelectorAll('[data-media-source="remote_video"]');
+    embeddedVideos.forEach(video => {
+      const videoURL = video.dataset.mediaSourceFileId;
+      const videoId = videoURL.split('/').pop();
+      video.innerHTML = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoId}" frameborder="0" allowfullscreen></iframe>`;
+    });
+  }, 100);
+}
